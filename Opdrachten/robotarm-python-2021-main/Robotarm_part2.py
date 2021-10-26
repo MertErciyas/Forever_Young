e6fb9c8fb3affef38f2d59070664e69d68d6279f
Mert Erciyas <99058235@mydavinci.nl>
2021-10-26 14:33:59 +0200

    Robot Arm Opdracht

---
+++ Opdrachten/robotarm-python-2021-main/Robotarm_part2.py
@@ -0,0 +1,35 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 2')
+
+# Jouw python instructies zet je vanaf hier:
+
+#first block
+robotArm.grab()
+
+for a in range (1,10):
+    robotArm.moveRight()
+
+robotArm.drop()
+
+for a in range (5,10):
+    robotArm.moveLeft()
+
+robotArm.grab()
+
+for a in range (5,10):
+    robotArm.moveRight()
+
+robotArm.drop()
+
+for a in range (8,10):
+    robotArm.moveLeft()
+
+robotArm.grab()
+for a in range (8,10):
+    robotArm.moveRight()
+
+robotArm.drop()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
---
+++ Opdrachten/robotarm-python-2021-main/Robotarm_part6.py
@@ -0,0 +1,19 @@
+
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 6')
+
+# Jouw python instructies zet je vanaf hier:
+for a in range (10):
+    robotArm.moveRight()
+
+for a in range(1,9):
+    robotArm.moveLeft()
+    robotArm.moveLeft()
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
